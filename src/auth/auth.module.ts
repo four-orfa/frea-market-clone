a729b116379406be5bdcb5ded1ce4d5c830ddbff
Toshinori Sumida <alias.ceuta@gmail.com>
2022-05-22 04:28:35 +0900

    jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
+import { JWT_SECRET_KEY } from 'config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: JWT_SECRET_KEY,
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
